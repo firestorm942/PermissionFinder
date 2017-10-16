f18b577315c52cd155ec392d096e80ac3686123d
joshua mims <speed9425@gmail.com>
2017-10-16 15:31:40 -0500

    update

--- src/me/HariboPenguin/PermissionFinder/PermissionFinder.java
+++ src/me/HariboPenguin/PermissionFinder/PermissionFinder.java
@@ -1,24 +1,16 @@
 package me.HariboPenguin.PermissionFinder;
 
-import java.io.IOException;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PermissionFinder extends JavaPlugin {
 
-    public FindCommand cmdExecutor = new FindCommand(this);
-    public DumpCommand dumpExecutor = new DumpCommand(this);
-    public String prefix = ChatColor.DARK_PURPLE + "[" + ChatColor.GOLD + "PermissionFinder" + ChatColor.DARK_PURPLE + "] ";
+    private FindCommand cmdExecutor = new FindCommand(this);
+    private DumpCommand dumpExecutor = new DumpCommand(this);
+    String prefix = ChatColor.DARK_PURPLE + "[" + ChatColor.GOLD + "PermissionFinder" + ChatColor.DARK_PURPLE + "] ";
 
     @Override
     public void onEnable() {
-
-        try {
-            Metrics metrics = new Metrics(this);
-            metrics.start();
-        } catch (IOException e) {
-            // Failed to submit the stats :-(
-        }
 
         getCommand("findperms").setExecutor(cmdExecutor);
         getCommand("dumpperms").setExecutor(dumpExecutor);
